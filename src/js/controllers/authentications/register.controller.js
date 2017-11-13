6e82519c31613decdf1e6fbb8c02d086b5ef72e6
wilsonespina <wilson.espina@gmail.com>
2017-11-13 11:34:47 +0000

    finished logout functionality and updated nav bar

--- src/js/controllers/authentications/register.controller.js
+++ src/js/controllers/authentications/register.controller.js
@@ -22,7 +22,7 @@ function registerController(
       .then(() => $auth.login(vm.member))
       .then(() => {
         currentUserService.getUser();
-        $state.go('membersIndex');
+        $state.go('login');
       });
   }
 }
