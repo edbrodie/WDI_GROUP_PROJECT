20fec9d0af8a0aa7e3150b77699f763d5df920aa
wilsonespina <wilson.espina@gmail.com>
2017-11-13 11:14:52 +0000

    updating the nav bar in index.html

--- db/seeds.js
+++ db/seeds.js
@@ -1,53 +1,53 @@
-// const mongoose = require('mongoose');
-// mongoose.Promise = require('bluebird');
-// const { dbUri } = require('../config/environment');
-//
-// mongoose.connect(dbUri, { useMongoClient: true });
-//
-// const Member = require('../models/member');
-// const Event = require('../models/event');
-//
-// //release model
-// Member.collection.drop();
-// Event.collection.drop();
-//
-// //Add Member models here
-// Member
-//   .create([{
-//     username: 'EddyB',
-//     email: 'ed@ed.com',
-//     password: 'password',
-//     passwordConfirmation: 'password'
-//   } ,
-//   {
-//     username: 'tom',
-//     email: 'tom@tom.com',
-//     password: 'password',
-//     passwordConfirmation: 'password'
-//   }])
-//   .then((members) => {
-//     console.log(`${members.length} new memebers added`);
-//     return Event
-//       .create([{
-//         createdBy: members [0],
-//         bandName: { type: String, trim: true, required: true },
-//         image: { type: String, trim: true, required: true },
-//         location: { type: String, trim: true, required: true },
-//         price: { type: String, trim: true, required: true }
-//       },{
-//         createdBy: members [0],
-//         bandName: { type: String, trim: true, required: true },
-//         image: { type: String, trim: true, required: true },
-//         location: { type: String, trim: true, required: true },
-//         price: { type: String, trim: true, required: true }
-//       },{
-//         createdBy: members [1],
-//         bandName: { type: String, trim: true, required: true },
-//         image: { type: String, trim: true, required: true },
-//         location: { type: String, trim: true, required: true },
-//         price: { type: String, trim: true, required: true }
-//       }]);
-//   })
-//   .then((events) => console.log(`${events.length} Cool events near you!`))
-//   .catch((err) => console.log(err))
-//   .finally(() => mongoose.connection.close());
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+const { dbUri } = require('../config/environment');
+
+mongoose.connect(dbUri, { useMongoClient: true });
+
+const Member = require('../models/member');
+const Event = require('../models/event');
+
+//release model
+Member.collection.drop();
+Event.collection.drop();
+
+//Add Member models here
+Member
+  .create([{
+    username: 'EddyB',
+    email: 'ed@ed.com',
+    password: 'password',
+    passwordConfirmation: 'password'
+  } ,
+  {
+    username: 'tom',
+    email: 'tom@tom.com',
+    password: 'password',
+    passwordConfirmation: 'password'
+  }])
+  .then((members) => {
+    console.log(`${members.length} new memebers added`);
+    return Event
+      .create([{
+        createdBy: members [0],
+        bandName: { type: String, trim: true, required: true },
+        image: { type: String, trim: true, required: true },
+        location: { type: String, trim: true, required: true },
+        price: { type: String, trim: true, required: true }
+      },{
+        createdBy: members [0],
+        bandName: { type: String, trim: true, required: true },
+        image: { type: String, trim: true, required: true },
+        location: { type: String, trim: true, required: true },
+        price: { type: String, trim: true, required: true }
+      },{
+        createdBy: members [1],
+        bandName: { type: String, trim: true, required: true },
+        image: { type: String, trim: true, required: true },
+        location: { type: String, trim: true, required: true },
+        price: { type: String, trim: true, required: true }
+      }]);
+  })
+  .then((events) => console.log(`${events.length} Cool events near you!`))
+  .catch((err) => console.log(err))
+  .finally(() => mongoose.connection.close());
