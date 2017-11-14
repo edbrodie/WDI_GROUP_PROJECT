a936126d3576a3413ecbc9e8ba77ed9c6773374f
djc74 <davidcooper974@gmail.com>
2017-11-14 09:42:45 +0000

    trying to sort show page

--- src/js/controllers/events/show-controller.js
+++ src/js/controllers/events/show-controller.js
@@ -2,18 +2,23 @@ angular
   .module('wdi-project-3')
   .controller('EventsShowCtrl', EventsShowCtrl);
 
-EventsShowCtrl.$inject = ['Event', '$stateParams'];
+EventsShowCtrl.$inject = ['Event', '$stateParams', '$http'];
 
-function EventsShowCtrl(Event, $stateParams) {
+function EventsShowCtrl(Event, $stateParams, $http) {
   const vm = this;
-  vm.event = Event.get($stateParams);
-
-  // vm.delete = event => {
-  //   Event
-  //     .remove({ id: event._id })
-  //     .$promise
-  //     .then(() => {
-  //       $state.go('evnetsIndex');
-  //     });
-  // };
+  // vm.event = Event.get($stateParams);
+  // console.log(vm.event.id);
+  Event
+    .get({ id: $stateParams.id })
+    .$promise
+    .then(data => {
+      vm.event = data;
+      $http
+        .getTicketmasterEvents()
+        .then(response => {
+          console.log(response.data);
+          vm.tmevent = response.data;
+          console.log(vm.tmevent);
+        });
+    });
 }
