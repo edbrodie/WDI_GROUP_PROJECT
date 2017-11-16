cf6f6df8174fe1dbc6d37edb6a681d144649761b
djc74 <davidcooper974@gmail.com>
2017-11-16 19:30:18 +0000

    index finished

--- src/js/controllers/events/index-controller.js
+++ src/js/controllers/events/index-controller.js
@@ -1,10 +1,15 @@
 angular.module('wdi-project-3').controller('EventsIndexCtrl', EventsIndexCtrl);
 
 EventsIndexCtrl.$inject = ['$scope', 'Event'];
-
 function EventsIndexCtrl($scope, Event) {
   const vm = this;
   vm.events = Event.query();
+
+  $scope.$parent.main.eventsIndex = true;
+
+  $scope.$on('$destroy', function(){
+    $scope.$parent.main.eventsIndex = false;
+  });
 
   Event.getTicketmasterEvents().$promise.then(data => {
     vm.events = data;
