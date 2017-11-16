cfe74eada7e1f2f0aea793e3995081219ac64cbc
wilsonespina <wilson.espina@gmail.com>
2017-11-16 00:23:25 +0000

    added loading gif

--- src/js/directives/google-map.directive.js
+++ src/js/directives/google-map.directive.js
@@ -4,9 +4,14 @@ angular
 
 googleMap.$inject = ['$window', '$timeout'];
 
+let map = null;
+let markers = [];
+let locations = null;
 
 
 function googleMap($window, $timeout) {
+
+
   return {
     restrict: 'E',
     replace: true,
@@ -22,15 +27,22 @@ function googleMap($window, $timeout) {
           lng: parseFloat(scope.center.lng)
         };
 
-        new $window.google.maps.Map(element[0], {
+        map = new $window.google.maps.Map(element[0], {
           zoom: 16,
           center: latLng
         });
 
+        addMarker();
         // create marker with latLng values
+        function addMarker() {
+          const marker = new google.maps.Marker({
+            position: latLng,
+            map: map,
+            icon: 'images/dot.svg'
+          });
 
-
-
+          markers.push(marker);
+        }
 
 
       }
