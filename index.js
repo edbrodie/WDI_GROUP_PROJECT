a83b44b81fba3f6463ed5e3e2473dbb87f95e261
wilsonespina <wilson.espina@gmail.com>
2018-01-04 15:25:06 +0000

    fixed

--- index.js
+++ index.js
@@ -1,40 +1,44 @@
-const express                 = require('express');
-const morgan                  = require('morgan');
-const bodyParser              = require('body-parser');
-const router                  = require('./config/routes');
-const { db, port, secret }    = require('./config/environment');
-const expressJWT              = require('express-jwt');
-const customResponses         = require('./lib/customResponses');
-const errorHandler            = require('./lib/errorHandler');
-const app                     = express();
-const environment             = app.get('env');
-const mongoose                = require('mongoose');
-const cors                    = require('cors');
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const router = require('./config/routes');
+const { db, port, secret } = require('./config/environment');
+const expressJWT = require('express-jwt');
+const customResponses = require('./lib/customResponses');
+const errorHandler = require('./lib/errorHandler');
+const app = express();
+const environment = app.get('env');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-mongoose.Promise              = require('bluebird');
+mongoose.Promise = require('bluebird');
 mongoose.connect(db[environment], { useMongoClient: true });
 
-if (app.get('env') !== 'production') app.use(cors());
+app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(`${__dirname}/public`));
 
-app.use('/api', expressJWT({ secret: secret })
-  .unless({
+app.use(
+  '/api',
+  expressJWT({ secret: secret }).unless({
     path: [
       { url: '/api/register', methods: ['POST'] },
-      { url: '/api/login',    methods: ['POST'] },
+      { url: '/api/login', methods: ['POST'] },
       { url: '/api/oauth/spotify', methods: ['POST'] }
     ]
-  }));
+  })
+);
 
 app.use(jwtErrorHandler);
 
-function jwtErrorHandler(err, req, res, next){
+function jwtErrorHandler(err, req, res, next) {
   if (err.name !== 'UnauthorizedError') return next();
-  return res.status(401).json({ message: 'You must be logged in to view this content' });
+  return res
+    .status(401)
+    .json({ message: 'You must be logged in to view this content' });
 }
 
 app.use(customResponses);
@@ -44,7 +48,9 @@ app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 app.use(errorHandler);
 
 if (environment !== 'test') {
-  app.listen(port, () => console.log(`Express is up and running on port: ${port}`));
+  app.listen(port, () =>
+    console.log(`Express is up and running on port: ${port}`)
+  );
 }
 
 module.exports = app;
