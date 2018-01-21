f8855b8741cca7ca500d70fb691e27c110c9355b
siobhanpotter <siobhanpotterone@gmail.com>
2018-01-21 21:12:44 +0000

    image change

--- src/js/constants/api.constants.js
+++ src/js/constants/api.constants.js
@@ -1,4 +1,4 @@
 angular
   .module('wdi-project-3')
-  // .constant('API', 'https://gig-event-app-siobhan.herokuapp.com/api');
-  .constant('API', 'https://localhost:8000');
+  .constant('API', 'https://gig-event-app-siobhan.herokuapp.com/api');
+// .constant('API', 'https://localhost:8000');
