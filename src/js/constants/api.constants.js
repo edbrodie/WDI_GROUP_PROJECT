6beb12fae3a1efc0cc090aa068962c3f2427133d
siobhanpotter <siobhanpotterone@gmail.com>
2018-01-21 21:09:40 +0000

    local host

--- src/js/constants/api.constants.js
+++ src/js/constants/api.constants.js
@@ -1,3 +1,4 @@
 angular
   .module('wdi-project-3')
-  .constant('API', 'https://gig-event-app-siobhan.herokuapp.com/api');
+  // .constant('API', 'https://gig-event-app-siobhan.herokuapp.com/api');
+  .constant('API', 'https://localhost:8000');
